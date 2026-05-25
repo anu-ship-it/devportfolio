40ffe45973cbda6983c8ed585e81bb1ad10c6488
anu-ship-it <anup17508@gmail.com>
2026-05-25 08:17:45 +0530

    Fixed some  lib dependencies error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import taildwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        Plugins: [taildwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
